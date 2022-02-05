6a263692b77e6a53f8f19a0d4e0b885c4aebf269
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-02-04 21:36:56 -0500

    commit tree c05b0b07a5418c804074d85dafdefc117b67a7a6

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() + 1);
+
+const num3 = 28
+const num4 = 6
+let mod = num3 % num4;
+
+
+const max = 20
+let theMax = Math.max();
+// describe('index.js', function() {
+//     describe('multiply', function() {
+//       it("is an equation whose multiplied results will equal 62", function() {
+//         expect(num1).to.be.a('number')
+//         expect(num2).to.be.a('number')
+//         expect(multiply).to.eq(62)
+//       });
+//     });
+//     describe('random', function() {
+//       it("generates a random integer greater than 0", function() {
+//         expect(Number.isInteger(random)).to.be.true
+//         expect(random).to.not.eq(0)
+//       });
+//     });
+//     describe('mod', function() {
+//       it("is an equation that calculates a remainder that is equal to 4", function() {
+//         expect(num3).to.be.a('number')
+//         expect(num4).to.be.a('number')
+//         expect(mod).to.eq(4)
+//       });
+//     });
+//     describe('max', function() {
+//       it("will return 20 as the highest number in the set", function() {
+//         expect(max).to.eq(20)
+//       });
+//     });
+//   });
